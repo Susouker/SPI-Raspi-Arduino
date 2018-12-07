19bea4716ab12d7f470817be1c34b5d33a90037a
Susouker <35164625+Susouker@users.noreply.github.com>
2018-12-07 22:03:04 +0100

    a

--- Master.py
+++ Master.py
@@ -2,5 +2,6 @@ import spidev
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1000000
-to_send = [0x01, 0x02, 0x03]
-spi.xfer(to_send)
+while true:
+    to_send = [0x01, 0x02, 0x03]
+    spi.xfer(to_send)
